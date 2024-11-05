db339e622629e60dcba41f7824adce7918f12f09
MrMubaroq <mubaroqahmad89@gmail.com>
2024-11-05 11:04:20 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function cekUsiaMengemudi(usia) {
+    if (usia >= 17) {
+      console.log("👌 Selamat Anda dapat mengemudi");
+    } else {
+      console.log("Mohon maaf anda belum bisa mengemudi");
+    }
+  }
+  
+  cekUsiaMengemudi(25);
+  cekUsiaMengemudi(15);
